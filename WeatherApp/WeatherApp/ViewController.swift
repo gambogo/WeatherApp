dbdc8cc3a49439011f12d32bb3f5eb030863ef14
Hao Tran <hao.trananh@nashtechglobal.com>
2021-10-30 00:33:29 +0700

    Init project

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Gambogo on 30/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
